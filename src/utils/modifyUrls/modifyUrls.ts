b6d9d928531d7ded4120c5df5bbfc56f9a82acab
Paul Schoen <paulschoen@rivian.com>
2025-01-20 19:58:33 -0600

    switching to instagramez

--- src/utils/modifyUrls/modifyUrls.ts
+++ src/utils/modifyUrls/modifyUrls.ts
@@ -1,9 +1,9 @@
 const urlPatterns = [
   // Disabling instagram replacement for now since the service is not available
-  // {
-  //   regex: /(?<!dd)instagram\.com/gi,
-  //   replacement: "g.ddinstagram.com",
-  // },
+  {
+    regex: /instagram\.com/gi,
+    replacement: "instagramez.com",
+  },
   {
     regex: /tiktok\.com/gi,
     replacement: "tnktok.com",
